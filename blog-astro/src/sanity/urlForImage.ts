7d3072578c7893e2cfe50bb42306c3414e205f04
Ted Marozzi <edwardmarozzi@gmail.com>
2023-08-28 18:48:57 +1000

    delete blog

--- blog-astro/src/sanity/urlForImage.ts
+++
@@ -1,9 +0,0 @@
-import { useSanityClient } from "@sanity/astro";
-import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
-
-export const imageBuilder = imageUrlBuilder(useSanityClient());
-
-export function urlForImage(source: SanityImageSource) {
-  return imageBuilder.image(source);
-}
