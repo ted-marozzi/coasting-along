543645cb1774641104c01830f8722a8e63094238
Ted Marozzi <edwardmarozzi@gmail.com>
2023-08-30 08:24:02 +1000

    site map

---
+++ blog/app/sitemap.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from "next";
+import { client } from "./sanity/client";
+
+export const baseUrl = "https://www.coastingalong.blog";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = (
+    await client.fetch<Array<{ slug: { current: string } }>>(
+      `*[_type == "post"]{
+      slug
+    }`,
+    )
+  ).map((post) => ({ url: `${baseUrl}/${post.slug.current}`, lastModified: new Date() }));
+
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/authors`,
+      lastModified: new Date(),
+    },
+    ...posts,
+  ];
+}
