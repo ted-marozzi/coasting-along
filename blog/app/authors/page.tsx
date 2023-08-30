0178a0d49817d4014a24bf06aec4928144bf92ed
Ted Marozzi <edwardmarozzi@gmail.com>
2023-08-30 16:32:51 +1000

    fix authors for small screens

--- blog/app/authors/page.tsx
+++ blog/app/authors/page.tsx
@@ -5,31 +5,36 @@ import { LightBoxImage } from "@/ui/lightBoxImage";
 import { PortableText } from "@portabletext/react";
 import { Metadata } from "next/types";
 
-type RouteParams = { params: { name: string } };
-
 export const metadata: Metadata = {
   title: "Authors | Coasting Along",
   description: "The Coasting Along author team",
 };
 
-export default async function Page({ params }: RouteParams) {
+export default async function Page() {
   const authors = (await client.fetch<Array<Author>>(`*[_type == "author"]`)).reverse();
+
   return (
     <>
       <div className="pb-2">
         <h2>Our Authors</h2>
       </div>
       {authors.map((author, i) => (
-        <div id={author.name} key={author._id} className="flex py-14">
-          {i % 2 === 0 ? (
-            <>
-              <AuthorImage author={author} /> <AuthorText author={author} />
-            </>
-          ) : (
-            <>
-              <AuthorText author={author} /> <AuthorImage author={author} />
-            </>
-          )}
+        <div key={author._id} id={author.name}>
+          <div className="hidden md:flex py-14">
+            {i % 2 === 0 ? (
+              <>
+                <AuthorImage author={author} /> <AuthorText author={author} />
+              </>
+            ) : (
+              <>
+                <AuthorText author={author} /> <AuthorImage author={author} />
+              </>
+            )}
+          </div>
+          <div className="flex flex-col md:hidden py-4">
+            <AuthorText author={author} />
+            <AuthorImage author={author} />
+          </div>
         </div>
       ))}
     </>
@@ -38,7 +43,7 @@ export default async function Page({ params }: RouteParams) {
 
 function AuthorText(props: { author: Author }) {
   return (
-    <div className="flex-1">
+    <div className="flex-1 p-2">
       <h4>{props.author.name}</h4>
       <PortableText value={props.author.bio} />
     </div>
@@ -47,7 +52,7 @@ function AuthorText(props: { author: Author }) {
 
 function AuthorImage(props: { author: Author }) {
   return (
-    <div className="flex-1 flex justify-center items-center">
+    <div className="flex-1 flex justify-center items-center p-2">
       <LightBoxImage
         src={urlFor(props.author.image).width(400).height(400).url()}
         lightBoxSrc={urlFor(props.author.image)
