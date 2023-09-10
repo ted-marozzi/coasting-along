584f635462d0152f69c00b3a2c757dd6763c9dc4
Ted Marozzi <edwardmarozzi@gmail.com>
2023-09-10 18:22:49 +1000

    Google Ads

--- blog/app/layout.tsx
+++ blog/app/layout.tsx
@@ -5,12 +5,18 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 import { Container } from "./ui/container";
 import { Content } from "./ui/content";
+import Script from "next/script";
 
 const font = Montserrat({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="light text-foreground bg-background">
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4754300691900449"
+        crossOrigin="anonymous"
+      />
       <body className={font.className}>
         <Providers>
           <Container>
