4f3a73d08928d15a9cb6c79aaf2ba987af5b752c
Ted Marozzi <edwardmarozzi@gmail.com>
2023-09-27 18:59:14 +1000

    Floating email subscribe

---
+++ blog/app/components/emailSubscriptionForms.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+export function FloatingEmailSubscriptionForm() {
+  const router = useRouter();
+  const key = "email-subscription-dismissed-last";
+  const lastDismissed = localStorage?.getItem(key) ?? null;
+
+  if (
+    process.env.NODE_ENV === "production" &&
+    lastDismissed !== null &&
+    new Date().getMilliseconds() - Date.parse(lastDismissed) < 32 * 24 * 60 * 60 * 1000
+  ) {
+    return <></>;
+  }
+
+  return (
+    <div className="fixed bottom-10 right-10 z-10 flex flex-col items-end">
+      <button
+        className="translate-y-5 -translate-x-2 z-20 hover:underline"
+        onClick={() => {
+          localStorage?.setItem(key, new Date().toString());
+          router.refresh();
+        }}
+      >
+        <svg
+          className="w-3 h-3 fill-secondary"
+          version="1.1"
+          id="Capa_1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          viewBox="0 0 460.775 460.775"
+          xmlSpace="preserve"
+        >
+          <path
+            d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55
+	c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55
+	c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505
+	c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55
+	l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719
+	c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"
+          />
+        </svg>
+      </button>
+      <div
+        className="ml-embedded shadow-2xl !bg-secondary !bg-opacity-20 !rounded-lg !border-2 !border-secondary"
+        data-form="15RKRt"
+      />
+    </div>
+  );
+}
--- blog/app/layout.tsx
+++ blog/app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 import { Container } from "./ui/container";
 import { Content } from "./ui/content";
 import Script from "next/script";
+import { FloatingEmailSubscriptionForm } from "./components/emailSubscriptionForms";
 
 const font = Montserrat({ subsets: ["latin"] });
 
@@ -17,12 +18,22 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4754300691900449"
         crossOrigin="anonymous"
       />
+      <Script id="mailer-lite-script">
+        {`
+    (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+    .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+    n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+    (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+    ml('account', '616096');
+`}
+      </Script>
       <body className={font.className}>
         <Providers>
           <Container>
             <Navbar />
             <Content>{children}</Content>
           </Container>
+          <FloatingEmailSubscriptionForm />
         </Providers>
       </body>
     </html>
