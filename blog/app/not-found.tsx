f195e79af2c85fadb2cd5c2ef6b77edfbd3501b4
Ted Marozzi <edwardmarozzi@gmail.com>
2023-10-08 08:17:05 +1100

    not found

--- blog/app/not-found.tsx
+++ blog/app/not-found.tsx
@@ -3,8 +3,8 @@ import { Link } from "@nextui-org/react";
 
 export default function NotFound() {
   return (
-    <div className="py-3 h-96">
-      <h1 className="my-6">Oh no!</h1>
+    <div className="p-3 h-96">
+      <h1 className="my-6">Oopsie x</h1>
       <p className="my-6">This page has gone missing.</p>
       <Link href="/" color="secondary">
         Return Home
